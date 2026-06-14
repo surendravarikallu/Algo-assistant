dec1acea9d7aaa0cb08c27580bad003a8b788f69
surendravarikallu <varikallusurendra@gmail.com>
2026-06-14 14:00:00 +0530

    feat: implement simulated transaction signing and state updates (fixes #1)

--- src/utils/mockAlgorandProvider.ts
+++ src/utils/mockAlgorandProvider.ts
@@ -12,4 +12,15 @@ export class MockAlgorandProvider {
   setBalance(address: string, amount: number) {
     this.balances[address] = amount;
   }
+
+  sendTransaction(txn: MockTransaction): string {
+    const senderBal = this.balances[txn.sender] || 0;
+    if (senderBal < txn.amount + txn.fee) {
+      throw new Error('Insufficient simulated balance');
+    }
+    this.balances[txn.sender] = senderBal - txn.amount - txn.fee;
+    this.balances[txn.receiver] = (this.balances[txn.receiver] || 0) + txn.amount;
+    this.blockHeight++;
+    return 'SIM_' + Math.random().toString(36).substr(2, 9);
+  }
 }
